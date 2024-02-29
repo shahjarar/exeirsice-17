1da5203306dfec1db33a82c75f9dc8d8d1136769
shahjarar <160387866+shahjarar@users.noreply.github.com>
2024-02-29 17:45:04 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,22 @@
+// Task 17
+// // initial list of guests
+// let guests:  string[] =["Ali" ,"Bilal" ,"Carry" ,"Dawood" ,"Hamza"];
+let  guest: string[] =["Ali","Bilal"];
+
+// // informing that only two people can be invited
+// // console.log("due to limited space , only two people can be invited for dinner.");
+
+// // // Removing guests until only two names remain
+// while (guests.length >2){
+
+//     const removedGuest = guests.pop(); // remove the last guest from the list
+// console.log('Sorry,${removedGuest}, you are no longer invited to dinner.');
+
+// //}
+
+// // Printing initation to the remaining two guests
+ console.log('Dear ${guest}, you are still invited to dinner.');
+ //});
+ // // removing the two names from the list
+ // guests.pop();
+ // guests.pop();
